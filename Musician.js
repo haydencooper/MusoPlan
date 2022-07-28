de3c68dccb8bde56d1a6e5dbf2a840ef66b76c62
unknown <hydnwc@gmail.com>
2022-07-29 07:50:59 +0800

    Created Musician.js

---
+++ Musician.js
@@ -0,0 +1,8 @@
+class Musician {
+    constructor(mname,minstrument,yrsplayed,rate) {
+        this.mname=mname;
+        this.minstrument=minstrument;
+        this.yrsplayed=yrsplayed;
+        this.rate=rate;
+    }
+}
