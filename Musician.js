dab6bca12c97f0b784df60751ec43caf3a549db8
haydencooper <109567870+haydencooper@users.noreply.github.com>
2022-08-04 09:27:07 +0800

    Delete Musician.js

--- Musician.js
+++
@@ -1,8 +0,0 @@
-class Musician {
-    constructor(mname,minstrument,yrsplayed,rate) {
-        this.mname=mname;
-        this.minstrument=minstrument;
-        this.yrsplayed=yrsplayed;
-        this.rate=rate;
-    }
-}
