409c7299916c984f7bb564af73a2d138cf39b647
Hayden Cooper <hydnwc@gmail.com>
2022-09-02 10:35:50 +0800

    Modified Musician.js

--- classes/Musician.js
+++ classes/Musician.js
@@ -1,18 +1,55 @@
-
 class Musician {
-     constructor(fullName, yearsPlaying, hourlyRate) { // Constructor for the Musician class
-      this.fullName = fullName; 
-      this.yearsPlaying = yearsPlaying; 
-      this.hourlyRate = hourlyRate; 
-      this.funFact = "";  
-    }
-    musicianIntroduction() { // Returns the musician's introduction
-      return `Hello there, my name is ${this.fullName} and I'm a ${this.musicianTypeName}.\
-  I currently play the ${this.instrumentName} and I have ${this.yearsPlaying} years of experience. \
-  It costs $${this.hourlyRate} per hour to hire me. \
-  A fun fact about my instrument is: '${this.funFact}'\
-  \n`;
+  constructor() {
+  }
+  set fullName(name) {
+    if (name.length >= 3 && name.length <= 30) {
+      this.name = name;
+    } else {
+      throw new Error("Name must be between 3 and 30 characters long");
     }
   }
-  
-  module.exports={Musician}; // Export the Musician class so it can be used in other files
+  getfullName() {
+    return this.name;
+  }
+
+  set hourlyRate(rate) {
+    if (rate >= 50) {
+      this.rate = rate;
+    } else {
+      throw new Error("Hourly rate must be equal to or greater than 50");
+    }
+  }
+  gethourlyRate() {
+    return this.rate;
+  }
+  set yearsPlaying(experience)
+  {
+    if (experience >= 0)
+    {
+      this.experience = experience;
+    } else {
+      throw new Error("Experience cannot be 0 or negative");
+    }
+  }
+  getYearsPlaying() 
+  {
+    return this.experience;
+  }
+  set musicianFact(funFact)
+  {
+    this.funFact = funFact
+  }
+  getfunFact()
+  {
+    return this.funFact
+  }
+
+  musicianIntroduction() {
+    // Returns the musician's introduction
+    return `Hello there, my name is ${this.name} and I'm a ${this.musicianTypeName}.\
+  I currently play the ${this.instrumentName} and I have ${this.experience} years of experience. \
+  It costs $${this.rate} per hour to hire me. \
+  A fun fact about my instrument is: ${this.funFact}`}
+}
+
+module.exports = { Musician }; // Export the Musician class so it can be used in other files
